b926ea98ad4dec5be865ddf41229b32b49c51b00
zren96 <zhiyir@princeton.edu>
2020-07-01 00:11:23 -0400

    use homing instead of open gripper, loop over grasping seq

--- scripts/graspEnv.py
+++ scripts/graspEnv.py
@@ -88,86 +88,70 @@ class GraspEnv(object):
 		start_joint_angles = [0.0, -0.785, 0.0, -2.356, 0.0, 1.571, 0.785]
 		self.pc.goto_joints(start_joint_angles)
 
-		print("============ Press Enter to move away from center...")
-		raw_input()
-		joint_angles = [-0.797, -0.137, 0.181, -2.608, 0.039, 2.472, -0.649]
-		self.pc.set_gripper(0.1)
-		self.pc.goto_joints(joint_angles)
+		# Keep grasping sep
+		while 1:
+			print("============ Press Enter to move away from center...")
+			raw_input()
+			joint_angles = [-0.797, -0.137, 0.181, -2.608, 0.039, 2.472, -0.649]
+			self.pc.home_gripper()
+			self.pc.goto_joints(joint_angles)
 
-		print("============ Press Enter to ask for grasp pose...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		res = self.grasp_infer_srv()
-		target_pos = array([res.pos.x, res.pos.y, res.pos.z])
-		# target_pos = array([res.pos.x, res.pos.y, res.pos.z+0.03]) # account for longer finger
-		target_yaw = res.yaw
-		print(target_pos, target_yaw)
+			print("============ Press Enter to ask for grasp pose...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			res = self.grasp_infer_srv()
+			target_pos = array([res.pos.x, res.pos.y, res.pos.z])
+			# target_pos = array([res.pos.x, res.pos.y, res.pos.z+0.03]) # account for longer finger
+			target_yaw = res.yaw
+			print(target_pos, target_yaw)
 
-		print("============ Press Enter to move to above target...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		target_pos_above = target_pos + array([0.,0.,0.10])
-		target_quat = quatMult(array([1.0, 0.0, 0.0, 0.0]), euler2quat([np.pi/4-target_yaw,0,0]))
-		target_above_pose =list(np.concatenate((target_pos_above, target_quat)))
-		self.pc.goto_pose(target_above_pose, velocity=0.20)
+			print("============ Press Enter to move to above target...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			target_pos_above = target_pos + array([0.,0.,0.10])
+			target_quat = quatMult(array([1.0, 0.0, 0.0, 0.0]), euler2quat([np.pi/4-target_yaw,0,0]))
+			target_above_pose =list(np.concatenate((target_pos_above, target_quat)))
+			self.pc.goto_pose(target_above_pose, velocity=0.20)
+			print(target_pos_above, array(self.robot_state.O_T_EE).reshape(4,4,order='F')[:3,-1])
 
-		print(target_pos_above, array(self.robot_state.O_T_EE).reshape(4,4,order='F')[:3,-1])
+			print("============ Press Enter to reach down...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			target_pose =list(np.concatenate((target_pos, target_quat)))
+			self.pc.goto_pose(target_pose, velocity=0.05)
 
-		print("============ Press Enter to reach down...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		target_pose =list(np.concatenate((target_pos, target_quat)))
-		self.pc.goto_pose(target_pose, velocity=0.05)
+			print("============ Press Enter to grasp...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			self.pc.grasp(width=0.00, e_inner=0.00, e_outer=0.005, speed=0.05, force=10)
 
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
+			print("============ Press Enter to lift...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			self.pc.goto_pose(target_above_pose, velocity=0.05)
 
-		print("============ Press Enter to grasp...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		self.pc.grasp(width=-0.01, e_inner=-0.01, e_outer=0.005, speed=0.03, force=10)
+			print("============ Press Enter to put down...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			self.pc.goto_pose(target_pose, velocity=0.05)		
+			self.pc.home_gripper()
 
-		print("============ Press Enter to lift...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		self.pc.goto_pose(target_above_pose, velocity=0.05)
+			print("============ Press Enter to lift and move back...")
+			raw_input()
+			if self.ROBOT_ERROR_DETECTED:
+				self.__recover_robot_from_error()
+			self.pc.goto_pose(target_above_pose, velocity=0.05)
+			joint_angles = [-0.797, -0.137, 0.181, -2.608, 0.039, 2.472, -0.649]
+			self.pc.goto_joints(joint_angles)
 
-		print("============ Press Enter to put down...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		self.pc.goto_pose(target_pose, velocity=0.05)		
-		self.pc.set_gripper(0.1)
-
-		print("============ Press Enter to lift and move back...")
-		raw_input()
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-		self.pc.goto_pose(target_above_pose, velocity=0.05)
-		# start_pose = [0.30, -0.30, 0.30, 1.0, 0.0, 0.0, 0.0]
-		joint_angles = [-0.797, -0.137, 0.181, -2.608, 0.039, 2.472, -0.649]
-		# self.pc.goto_pose(start_pose, velocity=0.25)
-		self.pc.goto_joints(joint_angles)
-
-		if self.ROBOT_ERROR_DETECTED:
-			self.__recover_robot_from_error()
-
-		# print("============ Press Enter to home...")
-		# raw_input()
-		# start_joint_angles = [0.0, -0.785, 0.0, -2.356, 0.0, 1.571, 0.785]
-		# self.pc.goto_joints(start_joint_angles)
-		# self.pc.set_gripper(0.1)
-
-		# if not grasp_ret or self.ROBOT_ERROR_DETECTED:
-		# 	rospy.logerr('Something went wrong, aborting this run')
-		# 	if self.ROBOT_ERROR_DETECTED:
-		# 		self.__recover_robot_from_error()
-		# 	continue
+			print("============ Press Enter to continue...")
+			raw_input()
 
 		return 1
 
